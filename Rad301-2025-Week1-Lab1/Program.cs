4e3838128b9799dd9115dbcf267bbb6281606059
SeanByrneORiain-STUD <SeanByrneORiain-STUD@S-8N453S3-SL12>
2025-09-15 12:54:56 +0100

    Add project files.

---
+++ Rad301-2025-Week1-Lab1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rad301_2025_Week1_Lab1
+{
+    public class Category
+    {
+        int CategoryID;
+        int Description;
+    }
+    public class Product
+    {
+        int ProductID;
+        string Description;
+        int QuantityInStock;
+        float UnityPrice;
+        int CategoryID;
+    }
+
+    public class Supplier
+    {
+        int SupplierID;
+        string SupplierName;
+        string SupplierAddress1;
+        string SupplierAddress2;
+    }
+
+    public class SupplierProduct
+    {
+        int SupplierID;
+        int ProductID;
+        DateTime DateFirstSupplied;
+    }
+
+    internal class ProductModel
+    { 
+
+    }
+}
