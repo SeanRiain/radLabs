4cf8f31113a0be9603f9a5d8778a5503e30de71b
Sean Byrne O Riain <S00254331@atu.ie>
2025-09-22 13:54:12 +0100

    RAD Lab 1 Simple Queries

--- Rad301-2025-Week1-Lab1/Program.cs
+++ Rad301-2025-Week1-Lab1/Program.cs
@@ -20,11 +20,11 @@ namespace Rad301_2025_Week1_Lab1
     }
     public class Product
     {
-        int ProductID;
-        string Description;
-        int QuantityInStock;
-        float UnityPrice;
-        int CategoryID;
+        public int ProductID;
+        public string Description;
+        public int QuantityInStock;
+        public float UnityPrice;
+        public int CategoryID;
 
         public Product(int ProductsID, string ProductsDescription, int ProductsQuantity, float ProductsPrice, int ProductsCategory)
         {
@@ -73,8 +73,8 @@ namespace Rad301_2025_Week1_Lab1
     {
         public static void Main(String[] args)
         {
-            Category ProductCategory;
-            Supplier ProductSupplier;
+            //Category ProductCategory;
+            //Supplier ProductSupplier;
 
             List<Supplier> Suppliers = new List<Supplier>();
             List<Product> Products = new List<Product>();
@@ -82,9 +82,8 @@ namespace Rad301_2025_Week1_Lab1
             List<SupplierProduct> SupplierProducts = new List<SupplierProduct>();
 
 
-
             Supplier Supplier1 = new Supplier(1, "ACME", "Collooney", "Sligo");
-            Supplier Supplier2 = new Supplier(1, "ACME", "Collooney", "Sligo");
+            Supplier Supplier2 = new Supplier(1, "Swift Electric", "Finglas", "Dublin");
 
             Product Product1 = new Product(1, "9 Inch Nails", 200, 0.1f, 1);
             Product Product2 = new Product(2, "9 Inch Bolts", 120, 0.2f, 1);
@@ -94,16 +93,50 @@ namespace Rad301_2025_Week1_Lab1
             Category Category1 = new Category(1, "Hardware");
             Category Category2 = new Category(2, "Electrical Appliances");
 
-            SupplierProduct SP1 = new SupplierProduct(1, 1, new DateTime(12, 12, 2012));
-            SupplierProduct SP2 = new SupplierProduct(1, 2, new DateTime(13, 08, 2017));
-            SupplierProduct SP3 = new SupplierProduct(1, 3, new DateTime(09, 09, 2022));
-            SupplierProduct SP4 = new SupplierProduct(1, 4, new DateTime(11, 04, 2024));
+            SupplierProduct SP1 = new SupplierProduct(1, 1, new DateTime(2012, 12, 12)); //YEAR MONTH DAY
+            SupplierProduct SP2 = new SupplierProduct(1, 2, new DateTime(2017, 08, 13)); 
+            SupplierProduct SP3 = new SupplierProduct(1, 3, new DateTime(2022, 09, 09));
+            SupplierProduct SP4 = new SupplierProduct(1, 4, new DateTime(2024, 04, 11));
 
             Suppliers.Add(Supplier1); Suppliers.Add(Supplier2);
             Products.Add(Product1); Products.Add(Product2); Products.Add(Product3); Products.Add(Product4);
             Categories.Add(Category1); Categories.Add(Category2);
             SupplierProducts.Add(SP1); SupplierProducts.Add(SP2); SupplierProducts.Add(SP3); SupplierProducts.Add(SP4);
 
+            //LINQ Queries
+
+            //All Categories
+            IEnumerable<Category> AllCategories =
+            from CategoryInstance in Categories
+            //where score > 80
+            select CategoryInstance;
+
+
+            //All Products
+            IEnumerable<Product> AllProducts =
+            from ProductInstance in Products
+            select ProductInstance;
+
+
+            //Products with a Quantity ≤ 100
+            IEnumerable<Product> ProductsUnder100 =
+            from ProductInstance in Products
+            where ProductInstance.QuantityInStock < 100
+            select ProductInstance;
+            foreach (Product product in ProductsUnder100)
+            {
+                Console.WriteLine(product.QuantityInStock);
+            }
+
+            //Products with a Quantity ≤ 120
+            IEnumerable<Product> ProductsOver120 =
+            from ProductInstance in Products
+            where ProductInstance.QuantityInStock > 120
+            select ProductInstance;
+            foreach (Product product in ProductsOver120)
+            {
+                Console.WriteLine(product.QuantityInStock);
+            }
         }
     }
 }
