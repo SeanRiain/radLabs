7940360893750e22a62f343b9b5b97c9e14fb2e1
Sean Byrne O Riain <S00254331@atu.ie>
2025-10-06 12:53:10 +0100

    Lab 2 Tutorial Section

---
+++ Rad301-2025-Week2-Lab2/ToDo.cs
@@ -0,0 +1,9 @@
+﻿namespace Rad301_2025_Week2_Lab2
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ Rad301-2025-Week2-Lab2/TodoDb.cs
@@ -0,0 +1,12 @@
+﻿namespace Rad301_2025_Week2_Lab2
+{
+    using Microsoft.EntityFrameworkCore;
+
+    class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options) { }
+
+        public DbSet<ToDo> Todos => Set<ToDo>();
+    }
+}
