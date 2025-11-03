3def53faaa94b706d681291c6d4adcc50da49609
Sean Byrne O Riain <S00254331@atu.ie>
2025-11-03 15:23:31 +0000

    Lab5part1 completed

---
+++ Lab5/Migrations/20251103152212_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab5.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    user_FirstName = table.Column<string>(type: "TEXT", maxLength: 15, nullable: false),
+                    user_LastName = table.Column<string>(type: "TEXT", maxLength: 15, nullable: false),
+                    title = table.Column<int>(type: "INTEGER", nullable: false),
+                    user_profile = table.Column<string>(type: "TEXT", nullable: false),
+                    age_of_user = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
