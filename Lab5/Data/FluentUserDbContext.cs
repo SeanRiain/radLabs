96a23f4951f2c92b59c9978ecb5d08742ee2db1e
Sean Byrne O Riain <S00254331@atu.ie>
2025-11-03 15:54:37 +0000

    Lab5Part2 Progress

---
+++ Lab5/Data/FluentUserDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Lab5;
+using Lab5.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Lab5.Data
+{
+    public class FluentUserDbContext : DbContext
+    {
+        public FluentUserDbContext(DbContextOptions<FluentUserDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FluentUser> FluentUsers { get; set; }
+    }
+}
---
+++ Lab5/Models/FluentUser .cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Lab5
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum FluentTitle
+    {
+        Mr,
+        Mrs,
+        Ms,
+        Dr,
+        Fr,
+        Rev,
+        Snr,
+        Jr,
+        Pvt,
+        Cpr,
+        Sgt,
+        Cpt,
+        Cmnd,
+        Prof,
+        HRH
+    }
+    public class FluentUser
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Title title { get; set; }
+        public string Biography { get; set; }
+        public int Age { get; set; }
+    }
+}
